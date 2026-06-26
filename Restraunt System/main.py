ce23ee241ed684602e640a44612f98a1abedb7e2
snishhhhh <snishhhhh@gmail.com>
2026-06-26 13:13:13 +0200

    Refactor restaurant management system code

--- Restraunt System/main.py
+++ Restraunt System/main.py
@@ -1,63 +1,199 @@
 from menu import Menu
-from customer_data import customer 
-from customer_data import registed_customer
-from reservation import Table_reservation
+from customer_data import Customer, registered_customers
+from reservation import TableReservation
 from generate_bill import bill
+from File_Managing_system import FileManager
 import sys
 
-Restaurent_menu= Menu()
-Customer= customer(None, "", "", "")
-Reservation= Table_reservation()
-Bill= bill()
+restaurant_menu = Menu()
+reservation = TableReservation()
+Bill = bill()
+file_manager = FileManager()
+
+
+file_manager.load_customers()
 
 while True:
-    print("==============RESTAURENT SYSTEM===============\n")
-    print("1. View Menu\n"
-          "2. Register a Customers\n"
-          "3. Make Reservation\n"
-          "4. Generate Bill")
-    choice= input("Please select the option number you want to execute: ")
-    if choice=="1":
-        Restaurent_menu.full_menu()
-    elif choice=="2":
-        name= input("Customer Name: ")
-        phone= input("Phone Number: ")
-        email= input("Email ID: ")
-        Customer.registration(name, phone, email)
-    elif choice=="3":
-        Reservation.make_reservation()
-    elif choice=="4":
-        answer= input(" Is the customer registred(Y/N): ")
-        if answer=="Y":
-            customer_id= int(input("Enter Customer ID: "))
-            if customer_id not in registed_customer:
-                print ("Customer ID not found")
+
+    print("\n RESTAURANT MANAGEMENT SYSTEM ")
+    print("1. View Menu")
+    print("2. Register Customer")
+    print("3. View Customer Details")
+    print("4. Make Reservation")
+    print("5. View Reservation")
+    print("6. Generate Bill")
+    print("7. View Customer Order History")
+    print("8. Search Menu Item")
+    print("9. Modify Menu Item")
+    print("10. Delete Menu Item")
+    print("11. Add Menu Item")
+    print("12. Save Customer Data")
+    print("13. Load Customer Data")
+    print("14. Exit")
+
+    choice = input("\nSelect an option: ")
+
+
+    if choice == "1":
+
+        restaurant_menu.full_menu()
+
+
+    elif choice == "2":
+
+        name = input("Customer Name: ")
+        phone = input("Phone Number: ")
+        email = input("Email Address: ")
+
+        customer = Customer(None, "", "", "")
+        customer.registration(name, phone, email)
+
+
+    elif choice == "3":
+
+        customer_id = int(input("Enter Customer ID: "))
+
+        if customer_id in registered_customers:
+
+            registered_customers[customer_id].display_customer()
+
+        else:
+
+            print("Customer not found.")
+
+
+    elif choice == "4":
+
+        reservation.make_reservation()
+
+
+    elif choice == "5":
+
+        reservation.view_reservation()
+
+
+    elif choice == "6":
+
+        answer = input("Is the customer registered? (Y/N): ").upper()
+
+        if answer == "Y":
+
+            customer_id = int(input("Enter Customer ID: "))
+
+            if customer_id not in registered_customers:
+
+                print("Customer not found.")
                 continue
-            Bill.creat_order_id(customer_id)
-            Restaurent_menu.full_menu()
-            
+
+            customer = registered_customers[customer_id]
+
+            Bill.create_order_id(customer_id)
+
+            restaurant_menu.full_menu()
+
             while True:
-                option=int(input("Enter the item number to add"))
-                if option==0:
+
+                option = int(input("Enter Item Number (0 to Finish): "))
+
+                if option == 0:
                     break
-                Bill.add_options_in_bill(option)
+
+                Bill.add_option_in_bill(option)
+
             Bill.print_bill()
-            Customer.reward_points(customer_id, Bill.total_bill)
-            Customer.add_order(Bill.order_id)
-                
-        elif answer=="N":
-            Bill.creat_order_id(None)
-            Restaurent_menu.full_menu()
+
+            customer.reward_points(Bill.total_bill)
+
+            customer.add_order(Bill.order_id)
+
+        elif answer == "N":
+
+            Bill.create_order_id(None)
+
+            restaurant_menu.full_menu()
+
             while True:
-                option=int(input("Enter the item number to add"))
-                if option==0:
+
+                option = int(input("Enter Item Number (0 to Finish): "))
+
+                if option == 0:
                     break
-                Bill.add_options_in_bill(option)
+
+                Bill.add_option_in_bill(option)
+
             Bill.print_bill()
+
+        else:
+
+            print("Invalid choice.")
             
-    else:
+
+    elif choice == "7":
+
+        customer_id = int(input("Enter Customer ID: "))
+
+        if customer_id in registered_customers:
+
+            registered_customers[customer_id].view_order_history()
+
+        else:
+
+            print("Customer not found.")
+
+
+    elif choice == "8":
+
+        option_id = int(input("Enter Item Number: "))
+
+        restaurant_menu.search_option(option_id)
+
+
+    elif choice == "9":
+
+        option_id = int(input("Enter Item Number: "))
+        new_name = input("Enter New Item Name: ")
+        new_price = float(input("Enter New Price: "))
+
+        restaurant_menu.modify_option(option_id, new_name, new_price)
+
+
+    elif choice == "10":
+
+        option_id = int(input("Enter Item Number: "))
+
+        restaurant_menu.delete_option(option_id)
+
+    elif choice == "11":
+
+        option_id = int(input("Enter New Item Number: "))
+        item_name = input("Enter Item Name: ")
+        price = float(input("Enter Item Price: "))
+
+        restaurant_menu.add_option(option_id, item_name, price)
+
+
+    elif choice == "12":
+
+        file_manager.save_customers()
+
+
+    elif choice == "13":
+
+        file_manager.load_customers()
+
+
+    elif choice == "14":
+
+        file_manager.save_customers()
+
+        print("\nCustomer data saved successfully.")
+        print("Thank you for using the Restaurant Management System.")
+
         sys.exit()
-            
+
+    else:
+
+        print("Invalid option. Please try again.")
         
     
             
